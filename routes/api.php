7ea8c13d5028a4705f105a8cb9f38daad67b1f61
yoshihama <yuya.soccer0224@gmail.com>
2023-02-06 01:00:04 +0900

    test 用 api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::middleware(['middleware' => 'api'])->group(function () {
+    Route::get('/users',  [UserController::class, 'index']);
 });
